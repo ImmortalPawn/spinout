fbe7b9d365dfd9e720a13833da579bded065fc3b
ImmortalPawn <gadzic.mladjan@gmail.com>
2017-12-07 23:18:30 +0100

    Source file for callback functions.

---
+++ src/callback.c
@@ -0,0 +1,22 @@
+#include "global.h"
+
+void on_keyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+    case 27:
+        /* ESC button - exiting game. */
+        exit(0);
+        break;
+	}
+
+	/* FIXME glutPostRedisplay(); ubaci kada bude imalo iscrtavanja */
+}
+
+void on_display(void)
+{
+    /* Clear previous buffer color. */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Send new picutre to window. */
+    glutSwapBuffers();
+}
