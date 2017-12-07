e4e3dc92cd04d64a2c128fd578fc53eccc4cad76
ImmortalPawn <gadzic.mladjan@gmail.com>
2017-12-07 23:21:44 +0100

    File cleaned from declarations and defeinitions of functions.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
 #include "global.h"
 #include "initialize.h"
 
-/* Deklaracije callback funkcija. */
-static void on_keyboard(unsigned char key, int x, int y);
-static void on_display(void);
-
 int main(int argc, char **argv)
 {
     /* GLUT init. */
@@ -29,23 +25,4 @@ int main(int argc, char **argv)
     return 0;
 }
 
-static void on_keyboard(unsigned char key, int x, int y)
-{
-    switch (key) {
-    case 27:
-        /* ESC button - exiting game. */
-        exit(0);
-        break;
-	}
 
-	/* FIXME glutPostRedisplay(); ubaci kada bude imalo iscrtavanja */
-}
-
-static void on_display(void)
-{
-    /* Clear previous buffer color. */
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    /* Send new picutre to window. */
-    glutSwapBuffers();
-}
