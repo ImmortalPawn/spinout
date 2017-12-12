6551a47cda75ca833f6f8be92154eee24fabc56a
ImmortalPawn <gadzic.mladjan@gmail.com>
2017-12-12 14:18:32 +0100

    Added degrees and radian functions.

--- src/global.c
+++ src/global.c
@@ -6,14 +6,10 @@ int window_height = 600;
 int timer_game_begin = 0;
 int is_active_timer_game_begin = 1;
 
-int timer_car_move = 1;
-int is_active_timer_car_move = 0;
-
-float car_rot = 0;
+int car_rot = 0;
 float car_speed = 0;
 float car_z_pos = 0;
 float car_y_pos = 0; 
-
 
 void print_car_speed()
 {
--- src/global.h
+++ src/global.h
@@ -1,11 +1,23 @@
 #ifndef _GLOBAL_H
 #define _GLOBAL_H
 
+#define _XOPEN_SOURCE 700
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
 
 #define PRINT_CAR_SPEED 1
+
+#ifndef M_PI
+#define M_PI 3.1415926535
+#endif
+
+/* Degrees to radians. */
+#define DEG_TO_RAD (1 / (180.0 / M_PI))
+/* Radians to degrees. */
+#define RAD_TO_DEG (180.0 * M_PI)
 
 /* Window width & height*/
 extern int window_width;
@@ -16,13 +28,8 @@ extern int timer_game_begin;
 /* Flag for timer_game_begin state. */
 extern int is_active_timer_game_begin;
 
-/* Value for timer_car_move. */
-extern int timer_car_move;
-/* Flag for timer_car_move state. */
-extern int is_active_timer_car_move;
-
 /* Car rotation. */
-extern float car_rot;
+int car_rot;
 /* Car speed atm. */
 extern float car_speed;
 /* Car coordinates. */
