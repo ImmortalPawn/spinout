309bed7901a93318a1554013576f5433df07f99b
ImmortalPawn <gadzic.mladjan@gmail.com>
2018-08-26 02:44:17 +0200

    Implemented coins, day and night shift. Improved visuals of objects.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include <GL/glut.h>
 
 /* Draw debug information. */
@@ -20,6 +21,12 @@ typedef struct _Car {
     GLfloat x;
     GLfloat y;
 } Car;
+
+typedef struct _Coin {
+    GLfloat x;
+    GLfloat y;
+    int isActive;
+} Coin;
 
 /* START of GLOBAL variables. */
 GLfloat carLength;
@@ -43,14 +50,31 @@ Line* lines;
 
 GLfloat linesLX;
 GLfloat linesRX;
+GLfloat linesSpawnY;
+
+int coinsNum;
+Coin* coins;
+GLfloat coinsSpawnY;
+GLfloat coinSize;
+GLfloat coinScaleY;
+GLfloat coinRotation;
+int coinPoints;
 
 int gameAnimation;
 
 int levelsNum;
 int* levels;
+GLfloat difficulty;
+char* playerName;
 GLfloat finalScore;
 GLfloat speed;
 GLfloat score;
+
+GLfloat lightX;
+GLfloat lightY;
+GLfloat lightZ;
+GLfloat lightSpeed;
+GLfloat isDay;
 /* END of GLOBAL variables.  */
 
 /* START of functions declarations. */
@@ -76,10 +100,11 @@ void onTimer(int timerId);
 /* Drawing functions. */
 void drawAxis(void);
 void drawEdges(void);
+void drawRoad(void);
 void drawRoadLines(void);
 void drawBotCars(void);
+void drawCoins(void);
 void drawPlayer(void);
-void drawRoad(void);
 /* END of functions declarations. */
 
 int main(int argc, char** argv)
@@ -121,51 +146,144 @@ void assert(int expr, char* msg)
 
 void initGlobalVars(int argc, char** argv)
 {
-    /* E - easy, M - medium, H - hard, I - insane. */
-    GLfloat difficulty;
-    if (argc == 2) {
-        switch (argv[1][0]) {
-            case 'e':
-            case 'E':
-                difficulty = 5.00f;
-                break;
-            case 'm':
-            case 'M':
-                difficulty = 4.00f;
-                break;
-            case 'h':
-            case 'H':
-                difficulty = 3.00f;
-                break;
-            case 'i':
-            case 'I':
-                difficulty = 2.50f;
-                break;
-            default:
-                difficulty = 5.00f;
+    /* Game difficulty: E - easy, M - medium, H - hard, I - insane. */
+    /* Set difficulty and player name based on command line arguments. */
+    if (argc == 1) {
+
+        difficulty = 5.00f;
+
+        int playerNameLen = strlen("unnamed");
+        playerName = (char*)malloc((playerNameLen + 1) * sizeof(char));
+        assert(NULL != playerName, "malloc()");
+
+        strcpy(playerName, "Unnamed");
+        playerName[playerNameLen] = '\0';
+    }
+    else if (argc == 2) {
+
+        int argvLen = strlen(argv[1]); 
+        if (argvLen == 1) {
+
+            switch (argv[1][0]) {
+
+                case 'e':
+                case 'E':
+                    difficulty = 5.00f;
+                    break;
+
+                case 'm':
+                case 'M':
+                    difficulty = 4.00f;
+                    break;
+
+                case 'h':
+                case 'H':
+                    difficulty = 3.00f;
+                    break;
+
+                case 'i':
+                case 'I':
+                    difficulty = 2.50f;
+                    break;
+
+                default:
+                    difficulty = 5.00f;
+            }
+
+            int playerNameLen = strlen("unnamed");
+            playerName = (char*)malloc((playerNameLen + 1) * sizeof(char));
+            assert(NULL != playerName, "malloc()");
+
+            strcpy(playerName, "Unnamed");
+            playerName[playerNameLen] = '\0';
+        }
+        else {
+
+            difficulty = 5.00f;
+
+            playerName = (char*)malloc((argvLen + 1) * sizeof(char));
+            assert(NULL != playerName, "malloc()");
+
+            strcpy(playerName, argv[1]);
+            playerName[argvLen] = '\0';
         }
     }
+    else if (argc == 3) {
+        
+        int argv1Len = strlen(argv[1]);
+
+        if (argv1Len == 1) {
+
+            switch (argv[1][0]) {
+
+                case 'e':
+                case 'E':
+                    difficulty = 5.00f;
+                    break;
+
+                case 'm':
+                case 'M':
+                    difficulty = 4.00f;
+                    break;
+
+                case 'h':
+                case 'H':
+                    difficulty = 3.00f;
+                    break;
+
+                case 'i':
+                case 'I':
+                    difficulty = 2.50f;
+                    break;
+
+                default:
+                    difficulty = 5.00f;
+            }
+        }
+        else {
+
+            difficulty = 5.00f;
+        }
+
+        int argv2Len = strlen(argv[2]);
+        playerName = (char*)malloc((argv2Len + 1) * sizeof(char));
+        assert(NULL != playerName, "malloc()");
+
+        strcpy(playerName, argv[2]);
+        playerName[argv2Len] = '\0';
+    }
     else {
+
         difficulty = 5.00f;
+        
+        int playerNameLen = strlen("unnamed");
+        playerName = (char*)malloc((playerNameLen + 1) * sizeof(char));
+        assert(NULL != playerName, "malloc()");
+
+        strcpy(playerName, "Unnamed");
+        playerName[playerNameLen] = '\0';
     }
 
-    carsNum = 10;
-
+    /* Initialize cars properties. */
     carScaleX = 0.70f;
     carLength = 0.20f;
     carWidth = carLength * carScaleX;
     carWheelSize = 0.03;
 
+    /* Initialize bot cars. */
+    carsNum = 10;
     cars = (Car*)malloc(carsNum * sizeof(Car));
     assert(NULL != cars, "malloc()");
 
-    /* Spawn points for bot cars. */
+    /* Distance between two bot cars. */
     GLfloat carsSeparation = 0.00f;
-    GLfloat carX = 0.00f;
-    int carRandomX = 0;
+
+    /* Initial bot cars locations. */
     for (int i = 0; i < carsNum; i++) {
 
-        carRandomX = rand() % 3;
+        GLfloat carX;
+        int carRandomX = rand() % 3;
+
         switch (carRandomX) {
             case 0:
                 carX = -0.50f;
@@ -183,8 +301,14 @@ void initGlobalVars(int argc, char** argv)
         carsSeparation += difficulty * carLength; 
     }
 
+    /* Spawn point for bot cars. */
     carsSpawnY = carsSeparation - 1.00f;
+    
+    #ifdef DEBUG_CONSOLE
+        printf("carsSeparation = %.2lf, carsSpawnY = %.2lf\n", carsSeparation, carsSpawnY);
+    #endif
 
+    /* Player car location. */
     playerCarX = 0.00f;
     playerCarY = -0.70f;
 
@@ -192,7 +316,7 @@ void initGlobalVars(int argc, char** argv)
         printf("carLength = %.2f, carWidth = %.2f, carsNum = %d\n", carLength, carWidth, carsNum);
     #endif
 
-    linesNum = 10;
+    /* Initialize road lines properties. */
     lineScaleX = 0.30f;
     lineLength = carLength / 2;
     lineWidth = lineLength * lineScaleX;
@@ -203,24 +327,96 @@ void initGlobalVars(int argc, char** argv)
         printf("lineLength = %.2f, lineWidth = %.2f, linesLX = %.2f, linesRX = %.2f\n", lineLength, lineWidth, linesLX, linesRX);
     #endif
 
+    /* Initialize road lines. */
+    linesNum = 10;
     lines = (Line*)malloc(linesNum * sizeof(Line));
     assert(NULL != lines, "malloc()");
 
-    /* Spawn points for road lines. */
+    /* Distance between road lines. */
     GLfloat linesSeparation = 0.00f;
+
+    /* Initial road lines locations. */
     for (int i = 0; i < linesNum; i++) {
+
         lines[i].y = 1.00f + lineLength/2 - linesSeparation;
         linesSeparation += 2 * lineLength; 
     }
+    
+    linesSpawnY = linesSeparation;
 
+    /* Initialize levels. */
     levelsNum = 10;
     levels = (int*)calloc(levelsNum, sizeof(int));
     assert(NULL != levels, "malloc()");
 
+    /* Coin properties. */
+    coinSize = carWidth / 2; 
+    coinScaleY = 0.20f;
+    coinRotation = 0.00f;
+    coinPoints = 5;
+
+    /* Initialize coins. */
+    coinsNum = carsNum;
+    coins = (Coin*)malloc(carsNum * sizeof(Coin));
+    assert(NULL != coins, "malloc()");
+
+    /* All coins are active at the beginning. */
+    for (int i = 0; i < coinsNum; i++) {
+
+        coins[i].isActive = 1;
+    }
+
+    /* Distance between two coins. */
+    GLfloat coinsSeparation = difficulty * carLength/2;
+
+    /* Initial coins locations. */
+    for (int i = 0; i < coinsNum; i++) {
+
+        GLfloat coinX;
+        int coinRandomX = rand() % 3;
+
+        switch (coinRandomX) {
+            case 0:
+                coinX = -0.50f;
+                break;
+            case 1:
+                coinX = 0.00f;
+                break;
+            case 2:
+                coinX = 0.50f;
+                break;
+        }
+
+        coins[i].x = coinX;
+        coins[i].y = 1.00f + coinsSeparation;
+        coinsSeparation += difficulty * carLength; 
+    }
+
+    /* Spawn point for coins. */
+    coinsSpawnY = coinsSeparation - 1.00f;
+
+    #ifdef DEBUG_CONSOLE
+        printf("coinsSeparation = %.2lf, coinsSpawnY = %.2lf\n", coinsSeparation, coinsSpawnY);
+    #endif
+
+    /* Indicator that shows if game should be animating. */
     gameAnimation = 0;
+
+    /* Speed of ingame objects. */
     speed = carLength / (GLfloat) GAME_TIMER_INTERVAL / 2;
+
+    /* Score based on distance traveled. */
     score = 0.00f;
+
+    /* Final score based on sum of score and number of coins collected. */
     finalScore = 0.00f;
+
+    /* Initial light position. */
+    lightX = 1.00f;
+    lightY = -0.99f;
+    lightZ = 0.60f;
+    lightSpeed = -0.002f;
+    isDay = 0;
 }
 
 void onKeyboard(unsigned char key, int x, int y)
@@ -295,9 +491,11 @@ void onDisplay(void)
     /* Clear buffers. */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /* Set point of view. */
+    /* Set model view. */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
+    /* Set point of view. */
     gluLookAt
     (
         0.00d, 0.00d, 1.00d,
@@ -332,6 +530,9 @@ void onDisplay(void)
 
     /* Draw bot cars. */
     drawBotCars();
+
+    /* Draw coins. */
+    drawCoins();
 
     /* Draw player car. */
     drawPlayer();
@@ -380,45 +581,82 @@ void onTimer(int timerId)
             }
         }
 
-        long int scoreBefore = (long int)score;
+        long int scoreBefore = (long int)finalScore;
         score += speed;
         finalScore += speed;
 
-        /* Print score only if long int value is different then previous one. (to reduce console spam). */
-        if (scoreBefore != (long int)score) {
+        coinRotation += 5.00f;
 
-            printf("Score: %ld\n", (long int)score);
+        /* Print score only if long int value is different then previous one. (to reduce console spam). */
+        if (scoreBefore != (long int)finalScore) {
+
+            printf("Score: %ld\n", (long int)finalScore);
         }
 
         /* Animate road lines. */
         for (int i = 0; i < linesNum; i++) {
+
             lines[i].y -= speed;
         }
 
         /* Animate bot cars. */
         for (int i = 0; i < carsNum; i++) {
+
             cars[i]. y -= speed;
         }
 
-        /* Force draw on display. */
-        glutPostRedisplay();
+        /* Animate coins. */
+        for (int i = 0; i < coinsNum; i++) {
 
-        /* Lines collision with bottom edge. */
+            coins[i].y -= speed;
+        }
+
+        /* Animate light to create day and night experience. */
+        lightY += lightSpeed;
+
+        if (lightY >= 0.99f) {
+
+            lightSpeed = -lightSpeed;
+            lightZ = -lightZ;
+            isDay = 1;
+        }
+
+        if (lightY < -0.99f) {
+
+            lightSpeed = -lightSpeed;
+            lightZ = -lightZ;
+            isDay = 0;
+        }
+
+        if (!isDay) {
+
+            glDisable(GL_LIGHT0);
+            glEnable(GL_LIGHT1);
+        }
+        else {
+
+            glEnable(GL_LIGHT0);
+            glDisable(GL_LIGHT1);
+        }
+
+        /* Road lines collision with bottom edge. */
         for (int i = 0; i < linesNum; i++) {
 
             if ((lines[i].y + lineLength/2) <= -1.00f) {
 
-                lines[i].y = 1.00f - lineLength/3;
+                lines[i].y = linesSpawnY - 1.00f - lineLength/2;
             }
         }
 
-        /* Bot cars colision with bottom edge. */
+        /* Bot cars collision with bottom edge. */
         for (int i = 0; i < carsNum; i++) {
+
+            int carRandomX;
+            GLfloat carX;
 
             if (cars[i].y + carLength/2 <= -1.00f) {
                 
-                int carRandomX = rand() % 3;
-                GLfloat carX;
+                carRandomX = rand() % 3;
 
                 switch (carRandomX) {
                     case 0:
@@ -438,6 +676,53 @@ void onTimer(int timerId)
             }
         }
 
+        /* Coins collision with bottom edge. */
+        for (int i = 0; i < coinsNum; i++) {
+
+            int coinRandomX; 
+            GLfloat coinX;
+
+            if (coins[i].y + coinSize/2 <= -1.00f) {
+                
+                coinRandomX = rand() % 3;
+
+                switch (coinRandomX) {
+                    case 0:
+                        coinX = -0.50f;
+                        break;
+                    case 1:
+                        coinX = 0.00f;
+                        break;
+                    case 2:
+                        coinX = 0.50f;
+                        break;
+                }
+
+                /* New spawn point for coin. */
+                coins[i].x = coinX;
+                coins[i].y = coinsSpawnY;
+                coins[i].isActive = 1;
+            }
+        }
+
+        /* TRICK: If coin spawns on top of bot car, remove it. */
+        for (int i = 0; i < coinsNum; i++) {
+
+            for (int j = 0; j < carsNum; j++) {
+
+                /* Collision detect. */
+                if (fabs((cars[j].y - coins[i].y)) <= carLength/2 + coinSize) {
+
+                    if (coins[i].isActive) {
+
+                        if (cars[j].x == coins[i].x) {
+                            coins[i].isActive = 0;
+                        }
+                    }
+                }
+            }
+        }
+
         /* Player car collision with bot cars. */
         for (int i = 0; i < carsNum; i++) {
 
@@ -449,13 +734,41 @@ void onTimer(int timerId)
 
                         if (cars[i].x == playerCarX) {
 
-                            printf("GAMEOVER: playerCarY = %.2f, colisionCarY = %.2f\n", playerCarY, cars[i].y);
+                            #ifdef DEBUG_CONSOLE
+                                printf("GAMEOVER: playerCarY = %.2f, colisionCarY = %.2f\n", playerCarY, cars[i].y);
+                            #endif
+                            printf("%s YOU LOSE!\nHIGHSCORE: \nYOUR SCORE: %ld\n", playerName, (long int)finalScore);
                             gameAnimation = 0;
                             /* onKeyboard(ESC, 0, 0); commented for debugging purposes. */
                         }
                 }
             }
         }
+
+        /* Player collision with coins. */
+        for (int i = 0; i < coinsNum; i++) {
+
+            /* Possible collision. */
+            if (coins[i].y <= -0.30f) {
+
+                /* Collision detect. */
+                if (fabs((coins[i].y - playerCarY)) <= carLength/2 + coinSize/2) {
+
+                        if (coins[i].x == playerCarX) {
+
+                            /* Remove coin because it is collected. */
+                            if (coins[i].isActive) {
+
+                                finalScore += coinPoints;
+                                coins[i].isActive = 0;
+                            }
+                        }
+                }
+            }
+        }
+
+        /* Force draw on display. */
+        glutPostRedisplay();
 
         if (gameAnimation) {
 
@@ -474,17 +787,21 @@ void initOpenGl(void)
 }
 
 void initLight(void)
-{
+{ 
     /* Light properties. */
-    GLfloat lightPosition[] = {1.00f, -0.99f, 0.60f, 1.00f};
+    GLfloat lightPosition[] = {lightX, lightY, lightZ, 1.00f};
     GLfloat lightAmbient[] = {0.20f, 0.20f, 0.20f, 1.00f};
     GLfloat lightDiffuse[] = {0.80f, 0.80f, 0.80f, 1.00f};
     GLfloat lightSpecular[] = {1.00f, 1.00f, 1.00f, 1.00f};
 
+    /* Car lights properties. */
+    GLfloat carLightsPosition[] = {playerCarX, playerCarY-carLength*5, 0.02f, 1.00f};
+    GLfloat carLightsAmbient[] = {0.40f, 0.40f, 0.00f, 1.00f};
+
     /* Material properties. */
-    GLfloat ambient_coeffs[] = {0.20f, 0.20f, 0.20f, 1.00f};
-    GLfloat diffuse_coeffs[] = {0.80f, 0.80f, 0.80f, 1.00f};
-    GLfloat specular_coeffs[] = {0.00f, 0.00f, 0.00f, 1.00f};
+    GLfloat ambientCoeffs[] = {0.20f, 0.20f, 0.20f, 1.00f};
+    GLfloat diffuseCoeffs[] = {0.80f, 0.80f, 0.80f, 1.00f};
+    GLfloat specularCoeffs[] = {0.00f, 0.00f, 0.00f, 1.00f};
     GLfloat shininess = 20.00f;
 
     /* Enable coloring. */
@@ -496,9 +813,6 @@ void initLight(void)
     /* Enable lighting. */
     glEnable(GL_LIGHTING);
 
-    /* Enable first light. */
-    glEnable(GL_LIGHT0);
-
     /* Apply light properties. */
     glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
@@ -506,10 +820,21 @@ void initLight(void)
     glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
 
     /* Apply material properties. */
-    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambientCoeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuseCoeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specularCoeffs);
     glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+
+    /* Apply player car lights properties. */
+    glLightfv(GL_LIGHT1, GL_AMBIENT, carLightsAmbient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, lightDiffuse);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, lightSpecular);
+    glLightfv(GL_LIGHT1, GL_POSITION, carLightsPosition);
+
+    /* Player car lights cutoff. */
+    glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 10.00f);
+    GLfloat spotDirection[] = {0.00f, 1.00f, 0.00f};
+    glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spotDirection);
 }
 
 void drawAxis(void)
@@ -609,9 +934,11 @@ void drawRoadLines(void)
 
 void deInitVars(void)
 {
+    free(playerName);
     free(cars);
     free(lines);
     free(levels);
+    free(coins);
 }
 
 void drawBotCars(void)
@@ -642,10 +969,9 @@ void drawBotCars(void)
             glutSolidSphere(0.10d, 50, 50);
         glPopMatrix();
 
-        glColor3f(0.00f, 0.00f, 0.00f); 
-
         /* Wheels. */
         /* Top left. */
+        glColor3f(0.00f, 0.00f, 0.00f); 
         glPushMatrix(); 
             glTranslatef(cars[i].x-carWidth/2, cars[i].y+carLength/3, 0.02f);
             glScalef(0.70f, 0.80f, 1.00f);
@@ -676,8 +1002,32 @@ void drawBotCars(void)
 
 }
 
+void drawCoins(void)
+{
+    for (int i = 0; i < coinsNum; i++) {
+
+        if (coins[i].isActive) {
+
+            glColor3f(0.88f, 0.91f, 0.20f);
+            glPushMatrix();
+                glTranslatef(coins[i].x, coins[i].y, 0.05f);
+
+                if (coinRotation >= 360.00f) {
+
+                    coinRotation = 0.00f;
+                }
+
+                glRotatef(coinRotation, 0.00f, 0.00f, 1.00f);
+                glScalef(1.00f, coinScaleY, 1.00f);
+                glutSolidSphere((GLdouble)coinSize, 50, 50);
+            glPopMatrix();
+        }
+    }
+}
+
 void drawPlayer(void)
 {
+
     /* Body. */ 
     glColor3f(0.00f, 0.00f, 1.00f); 
     glPushMatrix(); 
@@ -703,9 +1053,9 @@ void drawPlayer(void)
     glPopMatrix();
 
 
-    glColor3f(0.00f, 0.00f, 0.00f);
     /* Wheels. */
     /* Top left. */
+    glColor3f(0.00f, 0.00f, 0.00f);
     glPushMatrix(); 
         glTranslatef(playerCarX-carWidth/2, playerCarY+carLength/3, 0.02f);
         glScalef(0.70f, 0.80f, 1.00f);
